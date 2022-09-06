e4b87520413c2af62b51d4bf847c4d009819b39d
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-06 18:17:32 -0400

    Create migrations

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'main#index'
   resources :users
+  resources :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_192212) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_211946) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "entities", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_entities_on_user_id"
+  end
+
+  create_table "entities_groups", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "entity_id", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_192212) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "users"
+  add_foreign_key "groups", "users"
 end
