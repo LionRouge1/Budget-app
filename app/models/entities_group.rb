5587c42f7430f02719f7223b8f0e9b6a22c142c8
Masoudi Parrot <masoudi@localhost.localdomain>
2022-09-09 14:45:39 -0400

    add transaction requirement

---
+++ app/models/entities_group.rb
@@ -0,0 +1,9 @@
+class EntitiesGroup < ApplicationRecord
+    has_many :entities, dependent: :destroy
+    has_many :groups, dependent: :destroy
+
+    validates :group_id, presence: true, numericality: { only_integer: true,
+        greater_than_or_equal_to: 1 }
+    validates :entity_id, presence: true, numericality: { only_integer: true,
+        greater_than_or_equal_to: 1 }
+end
